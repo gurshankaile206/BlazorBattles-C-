7466bba633ca902ffdfedfbf8774df8fe8acee01
Gurshan Kaile <gurshankaile206@gmail.com>
2021-10-07 12:55:20 -0700

    add user authentication login and register

---
+++ BlazorBattles/Client/CustomAuthStateProvider.cs
@@ -0,0 +1,38 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Client
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorageService;
+
+        public CustomAuthStateProvider(ILocalStorageService localStorageService)
+        {
+            _localStorageService = localStorageService;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            if (await _localStorageService.GetItemAsync<bool>("isAuthenticated"))
+            {
+                var identity = new ClaimsIdentity(new[] {
+                    new Claim(ClaimTypes.Name, "Patrick")
+                }, "test authentication type");
+
+                var user = new ClaimsPrincipal(identity);
+                var state = new AuthenticationState(user);
+
+                NotifyAuthenticationStateChanged(Task.FromResult(state));
+
+                return state;
+            }
+            return new AuthenticationState(new ClaimsPrincipal());
+        }
+    }
+}
